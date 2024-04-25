3aa030aa2222bc56470027d343550a7891124f27
houckalexander <topspinhouck@gmail.com>
2024-04-24 20:28:24 -0400

    modified textcomplexity to output graphs - run_cli, cli, dependency, sentence, surface, utils.misc

--- personal/test.py
+++ personal/test.py
@@ -44,7 +44,7 @@ print(b)'''
 
 
 
-
+'''
 
 s = 'a a a a a a a a a a a a a a'
 a = s.split()
@@ -64,3 +64,25 @@ print(s)
 print(words[0])
 print(a)
 print(type_token)
+
+'''
+
+
+# importing required modules
+import argparse
+ 
+# create a parser object
+parser = argparse.ArgumentParser(description = "An addition program")
+ 
+# add argument
+parser.add_argument("add", nargs = '*', metavar = "num", type = int, 
+                     help = "All the numbers separated by spaces will be added.")
+ 
+# parse the arguments from standard input
+args = parser.parse_args()
+ 
+# check if add argument has any input data.
+# If it has, then print sum of the given numbers
+if len(args.add) != 0:
+    print(sum(args.add))
+
